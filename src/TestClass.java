86889c60252299f5e32d2199e34bf61658ff4a38
Hassan amanat Ali (U2389701) <U2389701@unimail.hud.ac.uk>
2024-01-30 15:44:36 +0000

    This is the first version of the ptojrct

---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+    public TestClass() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
---
+++ src/TestGUI.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class TestGUI {
+    private JButton button1;
+}
